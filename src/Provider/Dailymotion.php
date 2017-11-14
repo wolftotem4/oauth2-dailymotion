f4383bb358180031ec17dfd05c1fb43fa2039448
Randolph Huang <wolftotem4@gmail.com>
2017-11-14 17:51:49 +0800

    first commit

--- src/Provider/Dailymotion.php
+++ src/Provider/Dailymotion.php
@@ -1,10 +1,78 @@
 <?php
 
-
 namespace WTotem4\OAuth2\Client\Provider;
 
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
 
-class Dailymotion
+class Dailymotion extends AbstractProvider
 {
+    use BearerAuthorizationTrait;
 
+    /**
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://www.dailymotion.com/oauth/authorize';
+    }
+
+    /**
+     * @param  array  $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://api.dailymotion.com/oauth/token';
+    }
+
+    /**
+     * @param  \League\OAuth2\Client\Token\AccessToken  $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return 'https://api.dailymotion.com/user/me?fields=id,username,screenname,description,url,avatar_120_url';
+    }
+
+    /**
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * @param  \Psr\Http\Message\ResponseInterface  $response
+     * @param  array  $data
+     * @throws \League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (!empty($data['error'])) {
+            $code  = 0;
+            $error = $data['error'];
+
+            if (is_array($error)) {
+                $code  = $error['code'];
+                $error = $error['message'];
+            }
+
+            throw new IdentityProviderException($error, $code, $data);
+        }
+    }
+
+    /**
+     * @param  array  $response
+     * @param  \League\OAuth2\Client\Token\AccessToken  $token
+     * @return \WTotem4\OAuth2\Client\Provider\DailymotionUser
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new DailymotionUser($response);
+    }
 }
--- src/Provider/DailymotionUser.php
+++ src/Provider/DailymotionUser.php
@@ -1,10 +1,82 @@
 <?php
 
-
 namespace WTotem4\OAuth2\Client\Provider;
 
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
-class DailymotionUser
+class DailymotionUser implements ResourceOwnerInterface
 {
+    /**
+     * @var array
+     */
+    protected $data;
 
+    /**
+     * DailymotionUser constructor.
+     * @param array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->data = $response;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->getField('id');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUsername()
+    {
+        return $this->getField('username');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getScreenname()
+    {
+        return $this->getField('screenname');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAvatar()
+    {
+        return $this->getField('avatar_120_url');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getProfileUrl()
+    {
+        return $this->getField('url');
+    }
+
+    /**
+     * Get user data as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return mixed|null
+     */
+    private function getField($key)
+    {
+        return isset($this->data[$key]) ? $this->data[$key] : null;
+    }
 }
